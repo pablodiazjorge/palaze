bfc2691a356ed7fb87675c4133abeed8b3155a7a
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-03-21 12:47:24 +0100

    Header in progress..

--- stencil.config.ts
+++ stencil.config.ts
@@ -28,7 +28,6 @@ export const config: Config = {
         { src: 'pages/docs-tooltip.html', dest: 'docs-tooltip.html' },
         { src: 'pages/docs-spinner.html', dest: 'docs-spinner.html' },
         { src: 'pages/docs-design-system.html', dest: 'docs-design-system.html' },
-        { src: 'pages/docs-header.html', dest: 'docs-header.html' },
         { src: 'pages/docs-figure.html', dest: 'docs-figure.html' }
       ],
     },
