10a03d264f7d97c3c235069fc84a1e139dbe7133
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-04-05 16:36:17 +0200

    Updates in responsive layout.

--- stencil.config.ts
+++ stencil.config.ts
@@ -28,7 +28,6 @@ export const config: Config = {
         { src: 'pages/docs-tooltip.html', dest: 'docs-tooltip.html' },
         { src: 'pages/docs-spinner.html', dest: 'docs-spinner.html' },
         { src: 'pages/docs-design-system.html', dest: 'docs-design-system.html' },
-        { src: 'pages/docs-figure.html', dest: 'docs-figure.html' },
         { src: 'pages/docs-section.html', dest: 'docs-section.html' },
         { src: 'pages/docs-footer.html', dest: 'docs-footer.html' }
       ],
