652f407c0b9d2c477eba3051c6e2792f2bd5939f
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-04-05 11:47:52 +0200

    Responsive docs done, bug resize fixed. Table modifications to make it responsive. In need to redo all docs-sections files with the new modifications of header, sidebar and table.

--- src/components/plz-sidebar/plz-sidebar.tsx
+++ src/components/plz-sidebar/plz-sidebar.tsx
@@ -33,22 +33,22 @@ export class PlzSidebar {
   toggleDisabled(left: string, right: string) {
     this.toggled = false;
     document.getElementById('sidebar').style.width = '0';
-    document.getElementById(this.idMain).style.left = left;
-    document.getElementById(this.idMain).style.right = right;
+    document.getElementById(this.idMain).style.marginLeft = left;
+    document.getElementById(this.idMain).style.marginRight = right;
   }
 
   toggleActivated(left: string, right: string) {
     this.toggled = true;
     document.getElementById('sidebar').style.width = `${this.width}px`;
-    document.getElementById(this.idMain).style.left = left;
-    document.getElementById(this.idMain).style.right = right;
+    document.getElementById(this.idMain).style.marginLeft = left;
+    document.getElementById(this.idMain).style.marginRight = right;
   }
 
   render() {
     return (
       <Host>
         <button class="button-sidebar" style={{ 'margin-top': `${this.spaceTop}px` }} onClick={() => this.toggle()} innerHTML="&#9776"></button>
-        <nav id="sidebar" class="sidebar" style={{ 'width': `${this.width}px`, '--bg-color': `${this.color}`, 'margin-top': `${this.spaceTop}px` }}>
+        <nav id="sidebar" class="sidebar" style={{ 'width': `${this.width}px`, '--bg-color': `${this.color}`}}>
           <slot></slot>
         </nav>
       </Host>
