f258cdcb1ac29e02aa2682c375a99a87cc5397b1
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-05-06 14:54:37 +0200

    All code-optimization and comments in css and tsx files done. Read me updates done.

--- src/components/plz-button/readme.md
+++ src/components/plz-button/readme.md
@@ -6,7 +6,7 @@ Los componentes button representan acciones accesibles desde un click por el usu
 
 | Nombre        | Descripción                                                | Posibilidades                                                    | Por defecto |
 | ------------- | ---------------------------------------------------------- | ---------------------------------------------------------------- | ----------- |
-| variant       | Distintas variantes del componente                         | 'default' - 'icon' - 'progress' - 'shadow'                       | 'default'   |
+| variant       | Distintas variantes del componente                         | 'default' - 'icon' - 'progress'                       | 'default'   |
 | link          | Url a la que se accedera a partir del componente           | 'docs-ejemplo.html'                                              |             |
 | target        | Distintas formas de mostar el link del componente          | '_self' - '_blank' - '_parent' - '_top' - 'framename'            | '_self'     |
 | color         | Distintos colores del componente                            | 'default' - 'black' - 'purple' - 'blue' - 'green' - 'red' - 'orange' - 'yellow' | 'default' |
@@ -15,7 +15,7 @@ Los componentes button representan acciones accesibles desde un click por el usu
 | shadow-color  | Distintos colores de sombra para el componenten              | 'black' - 'purple' - 'blue' - 'green' - 'red' - 'orange' - 'yellow' |             |
 | size          | Distintos tamaños del componente botón                      | 'default' - 'small' - 'big'                                       | 'default'   |
 | corners       | Distintas formas de las esquinas del componente             | 'default' - 'low-rounded' - 'rounded' - 'square' - 'circle'      | 'default'   |
-| icon          | Solo compatible con la variante "icon". Distintos iconos para la variante | 'github' - 'linkedin' - 'mail' - 'palaze'                  |             |
+| icon          | Solo compatible con la variante "icon". Distintos iconos para la variante | 'github' - 'linkedin' - 'mail' - 'palaze' - '..url..'                  |             |
 
 
 --------------------------------------------------------------------------------------------------------------
--- src/components/plz-dropdown-menu/plz-dropdown-menu.tsx
+++ src/components/plz-dropdown-menu/plz-dropdown-menu.tsx
@@ -6,123 +6,156 @@ import { Component, Host, Prop, h } from '@stencil/core';
   shadow: true,
 })
 export class PlzDropdownMenu {
-  @Prop() variant: string = 'simple';
-  @Prop() active: boolean = false;
-  @Prop() link: string = '';
-  @Prop() target: string = '_self';
-  @Prop() colorHover: string = '';
-  @Prop() colorText: string = 'white';
-  @Prop() static: boolean = false;
+  @Prop() variant: 'simple' | 'custom' = 'simple'; //Distintas variables del componente
+  @Prop({ mutable: true }) active: boolean = false; //booleano para saber si el item está activado (cambio en color)
+  @Prop() link?: string; //url al enlace
+  @Prop() target?: '_self' | '_blank' | '_parent' | '_top' | 'framename' = '_self'; //Forma de acceder a la url
+  @Prop() colorHover: string = ''; //Color del item al pasar el cursor por encima
+  @Prop() colorText: string = 'white'; //color del texto al hacer hover
+  @Prop() static: boolean = false; //booleano para mantener fijo el dropdown
 
+  /**
+   * Función que cambia el estado de la propiedad active
+   */
   changeActive() {
     this.active = !this.active;
   }
 
   render() {
-    return this.variant == 'simple' && this.link != '' && this.static ? (
-      <Host>
-        <li>
-          <a href={this.link} target={this.target} class={{ simple: true, item: true }}>
-            <slot></slot>
-          </a>
-          <ul class={{ dropdown: this.static }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'simple' && this.link != '' ? (
-      <Host>
-        <li>
-          <a href={this.link} target={this.target} class={{ simple: true, item: true, selected: this.active }} onClick={() => this.changeActive()}>
-            <slot></slot>
-          </a>
-          <ul class={{ dropdown: this.active }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'simple' && this.static ? (
-      <Host>
-        <li>
-          <a class={{ simple: true, item: true}}>
-            <slot></slot>
-          </a>
-          <ul id="#sub-menu" class={{ dropdown: this.static }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'simple' ? (
-      <Host>
-        <li>
-          <a class={{ simple: true, item: true, selected: this.active }} onClick={() => this.changeActive()}>
-            <slot></slot>
-          </a>
-          <ul id="#sub-menu" class={{ dropdown: this.active }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'custom' && this.link != '' && this.static ? (
-      <Host>
-        <li>
-          <a
-            href={this.link} target={this.target}
-            class={{ custom: true, item: true }}
-            style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
-          >
-            <slot></slot>
-          </a>
-          <ul id="#sub-menu" class={{ dropdown: this.static }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'custom' && this.link != '' ? (
-      <Host>
-        <li>
-          <a
-            href={this.link} target={this.target}
-            class={{ custom: true, item: true, active: this.active }}
-            style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
-            onClick={() => this.changeActive()}
-          >
-            <slot></slot>
-          </a>
-          <ul id="#sub-menu" class={{ dropdown: this.active }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'custom' && this.static ? (
-      <Host>
-        <li>
-          <a
-            class={{ custom: true, item: true }}
-            style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
-          >
-            <slot></slot>
-          </a>
-          <ul id="#sub-menu" class={{ dropdown: this.static }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : this.variant == 'custom' ? (
-      <Host>
-        <li>
-          <a
-            class={{ custom: true, item: true, active: this.active }}
-            style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
-            onClick={() => this.changeActive()}
-          >
-            <slot></slot>
-          </a>
-          <ul id="#sub-menu" class={{ dropdown: this.active }}>
-            <slot name="dropdown"></slot>
-          </ul>
-        </li>
-      </Host>
-    ) : null;
+    switch (this.variant) {
+      /**
+       * Variante simple
+       * Dropdown, estilo simple con cuatro posibilidades
+       *    Dropdown estatico, link
+       *    Link
+       *    Estatico
+       *    No link, no estatico
+       */
+      case 'simple':
+        return this.link != '' && this.static ? (
+          /* Dropdown estatico, link */
+          <Host>
+            <li>
+              <a href={this.link} target={this.target} class={{ simple: true, item: true }}>
+                <slot></slot>
+              </a>
+              <ul class={{ dropdown: this.static }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        ) : this.link != '' ? (
+          /* Link */
+          <Host>
+            <li>
+              <a href={this.link} target={this.target} class={{ simple: true, item: true, selected: this.active }} onClick={() => this.changeActive()}>
+                <slot></slot>
+              </a>
+              <ul class={{ dropdown: this.active }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        ) : this.static ? (
+          /* Estatico */
+          <Host>
+            <li>
+              <a class={{ simple: true, item: true }}>
+                <slot></slot>
+              </a>
+              <ul id="#sub-menu" class={{ dropdown: this.static }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        ) : (
+          /* No link, no estatico */
+          <Host>
+            <li>
+              <a class={{ simple: true, item: true, selected: this.active }} onClick={() => this.changeActive()}>
+                <slot></slot>
+              </a>
+              <ul id="#sub-menu" class={{ dropdown: this.active }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        );
+      case 'custom':
+      /**
+       * Variante custom
+       * Dropdown, estilo propio palaze con cuatro posibilidades
+       *    Dropdown estatico, link
+       *    Link
+       *    Estatico
+       *    No link, no estatico
+      */
+        return this.link != '' && this.static ? (
+          /* Dropdown estatico, link */
+          <Host>
+            <li>
+              <a
+                href={this.link}
+                target={this.target}
+                class={{ custom: true, item: true }}
+                style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
+              >
+                <slot></slot>
+              </a>
+              <ul id="#sub-menu" class={{ dropdown: this.static }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        ) : this.link != '' ? (
+          /* Link */
+          <Host>
+            <li>
+              <a
+                href={this.link}
+                target={this.target}
+                class={{ custom: true, item: true, active: this.active }}
+                style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
+                onClick={() => this.changeActive()}
+              >
+                <slot></slot>
+              </a>
+              <ul id="#sub-menu" class={{ dropdown: this.active }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        ) : this.static ? (
+          /* Estatico */
+          <Host>
+            <li>
+              <a class={{ custom: true, item: true }} style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}>
+                <slot></slot>
+              </a>
+              <ul id="#sub-menu" class={{ dropdown: this.static }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        ) : (
+          /* No link, no estatico */
+          <Host>
+            <li>
+              <a
+                class={{ custom: true, item: true, active: this.active }}
+                style={{ '--bg-color-hover': `${this.colorHover}`, '--bg-color-text': `${this.colorText}` }}
+                onClick={() => this.changeActive()}
+              >
+                <slot></slot>
+              </a>
+              <ul id="#sub-menu" class={{ dropdown: this.active }}>
+                <slot name="dropdown"></slot>
+              </ul>
+            </li>
+          </Host>
+        );
+      default:
+        null;
+    }
   }
 }
